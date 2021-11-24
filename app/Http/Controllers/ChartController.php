6295e426bf5287d8e4e33c19110a2931dfaa26b7
rizman768 <rizqimaulana768@gmail.com>
2021-11-24 14:53:44 +0700

    chart berdasar dev

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -9,24 +9,69 @@ use Illuminate\Support\Facades\DB;
 class ChartController extends Controller
 {
     public function index(){
-    	$suhus = DB::table('temp_suhus')->get();
+    	$suhus1 = DB::table('temp_suhus')->where('dev_id', '=', 'lht65-1')->get();
 
-    	$labels = [];
-    	$data1 = [];
-        $data2 = [];
-        $data3 = [];
-        $data4 = [];
+    	$labels1 = [];
+    	$data11 = [];
+        $data21 = [];
+        $data31 = [];
+        $data41 = [];
 
-    	foreach ($suhus as $suhu) {
-    		$labels[] = $suhu->app_id;
-            $data1[] = $suhu->hum_sht;
-            $data2[] = $suhu->tempc_ds;
-            $data3[] = $suhu->tempc_sht;
+    	foreach ($suhus1 as $suhu) {
+    		$labels1[] = $suhu->app_id;
+            $data11[] = $suhu->hum_sht;
+            $data21[] = $suhu->tempc_ds;
+            $data31[] = $suhu->tempc_sht;
     	}
+
+        $suhus2 = DB::table('temp_suhus')->where('dev_id', '=', 'lht65-2')->get();
+
+        $labels2 = [];
+        $data12 = [];
+        $data22 = [];
+        $data32 = [];
+        $data42 = [];
+
+        foreach ($suhus2 as $suhu) {
+            $labels2[] = $suhu->app_id;
+            $data12[] = $suhu->hum_sht;
+            $data22[] = $suhu->tempc_ds;
+            $data32[] = $suhu->tempc_sht;
+        }
+
+        $suhus3 = DB::table('temp_suhus')->where('dev_id', '=', 'lht65-3')->get();
+
+        $labels3 = [];
+        $data13 = [];
+        $data23 = [];
+        $data33 = [];
+        $data43 = [];
+
+        foreach ($suhus3 as $suhu) {
+            $labels3[] = $suhu->app_id;
+            $data13[] = $suhu->hum_sht;
+            $data23[] = $suhu->tempc_ds;
+            $data33[] = $suhu->tempc_sht;
+        }   
+
+        $suhus4 = DB::table('temp_suhus')->where('dev_id', '=', 'outdoor_temperature1')->get();
+
+        $labels4 = [];
+        $data14 = [];
+        $data24 = [];
+        $data34 = [];
+        $data44 = [];
+
+        foreach ($suhus4 as $suhu) {
+            $labels4[] = $suhu->app_id;
+            $data14[] = $suhu->hum_sht;
+            $data24[] = $suhu->tempc_ds;
+            $data34[] = $suhu->tempc_sht;
+        }
 
     	// dd(json_encode($data1));
 
-    	return view('chart.index',['labels' => $labels, 'data1' => $data1, 'data2' => $data2, 'data3' => $data3]);
+    	return view('chart.index',['labels1' => $labels1, 'data11' => $data11, 'data21' => $data21, 'data31' => $data31, 'labels2' => $labels2, 'data12' => $data12, 'data22' => $data22, 'data32' => $data32, 'labels3' => $labels3, 'data13' => $data13, 'data23' => $data23, 'data33' => $data33, 'labels4' => $labels4, 'data14' => $data14, 'data24' => $data24, 'data34' => $data34]);
     }
 
     public function dashboard(){
--- resources/views/chart/index.blade.php
+++ resources/views/chart/index.blade.php
@@ -17,7 +17,61 @@
                                     </div>
                                 </div>
                                 <div class="panel-body">
-                                    <div id="chart" class="ct-chart"></div>
+                                    <div id="chart1" class="ct-chart"></div>
+                                </div>
+                            </div>
+                            <!-- END VISIT CHART -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- VISIT CHART -->
+                            <div class="panel">
+                                <div class="panel-heading">
+                                    <h1 class="panel-title">Temperature Suhu</h1>
+                                    <div class="right">
+                                        <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+                                        <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+                                    </div>
+                                </div>
+                                <div class="panel-body">
+                                    <div id="chart2" class="ct-chart"></div>
+                                </div>
+                            </div>
+                            <!-- END VISIT CHART -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- VISIT CHART -->
+                            <div class="panel">
+                                <div class="panel-heading">
+                                    <h1 class="panel-title">Temperature Suhu</h1>
+                                    <div class="right">
+                                        <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+                                        <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+                                    </div>
+                                </div>
+                                <div class="panel-body">
+                                    <div id="chart3" class="ct-chart"></div>
+                                </div>
+                            </div>
+                            <!-- END VISIT CHART -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- VISIT CHART -->
+                            <div class="panel">
+                                <div class="panel-heading">
+                                    <h1 class="panel-title">Temperature Suhu</h1>
+                                    <div class="right">
+                                        <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+                                        <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+                                    </div>
+                                </div>
+                                <div class="panel-body">
+                                    <div id="chart4" class="ct-chart"></div>
                                 </div>
                             </div>
                             <!-- END VISIT CHART -->
@@ -31,7 +85,7 @@
 @section('chart')
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script>
-	Highcharts.chart('chart', {
+	Highcharts.chart('chart1', {
         chart: {
             type: 'column'
         },
@@ -39,22 +93,22 @@
             text: 'Temperature Suhu'
         },
         subtitle: {
-            text: 'Source: WorldClimate.com'
+            text: 'Dev Id : lht165-1'
         },
         xAxis: {
-            categories: {!! json_encode($labels) !!},
+            categories: {!! json_encode($labels1) !!},
             crosshair: true
         },
         yAxis: {
             min: 0,
             title: {
-                text: 'Rainfall (mm)'
+                text: 'Rainfall (celcius)'
             }
         },
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f} celcius</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
@@ -67,144 +121,164 @@
         },
         series: [{
             name: 'Hum SHT',
-            data: {!! json_encode($data1) !!}
+            data: {!! json_encode($data11) !!}
 
         }, {
             name: 'TEMPC DS',
-            data: {!! json_encode($data2) !!}
+            data: {!! json_encode($data21) !!}
 
         }, {
             name: 'TEMPC SHT',
-            data: {!! json_encode($data3) !!}
+            data: {!! json_encode($data31) !!}
+
+        }]
+    });
+
+    Highcharts.chart('chart2', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Temperature Suhu'
+        },
+        subtitle: {
+            text: 'Dev Id : lht165-2'
+        },
+        xAxis: {
+            categories: {!! json_encode($labels2) !!},
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Rainfall (celcius)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} celcius</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Hum SHT',
+            data: {!! json_encode($data12) !!}
+
+        }, {
+            name: 'TEMPC DS',
+            data: {!! json_encode($data22) !!}
+
+        }, {
+            name: 'TEMPC SHT',
+            data: {!! json_encode($data32) !!}
+
+        }]
+    });
+
+    Highcharts.chart('chart3', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Temperature Suhu'
+        },
+        subtitle: {
+            text: 'Dev Id : lht165-3'
+        },
+        xAxis: {
+            categories: {!! json_encode($labels3) !!},
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Rainfall (celcius)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} celcius</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Hum SHT',
+            data: {!! json_encode($data13) !!}
+
+        }, {
+            name: 'TEMPC DS',
+            data: {!! json_encode($data23) !!}
+
+        }, {
+            name: 'TEMPC SHT',
+            data: {!! json_encode($data33) !!}
+
+        }]
+    });
+
+    Highcharts.chart('chart4', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Temperature Suhu'
+        },
+        subtitle: {
+            text: 'outdoor_temperature1'
+        },
+        xAxis: {
+            categories: {!! json_encode($labels4) !!},
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Rainfall (celcius)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} celcius</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Hum SHT',
+            data: {!! json_encode($data14) !!}
+
+        }, {
+            name: 'TEMPC DS',
+            data: {!! json_encode($data24) !!}
+
+        }, {
+            name: 'TEMPC SHT',
+            data: {!! json_encode($data34) !!}
 
         }]
     });
 </script>
-@endsection
-@section('chart1')
-    <script>
-    $(function() {
-        var data, options;
-
-        // headline charts
-        data = {
-            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            series: [
-                [23, 29, 24, 40, 25, 24, 35],
-                [14, 25, 18, 34, 29, 38, 44],
-            ]
-        };
-
-        options = {
-            height: 300,
-            showArea: true,
-            showLine: false,
-            showPoint: false,
-            fullWidth: true,
-            axisX: {
-                showGrid: false
-            },
-            lineSmooth: false,
-        };
-
-        new Chartist.Line('#chart', data, options);
-
-
-        // visits trend charts
-        data = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [{
-                name: 'series-real',
-                data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
-            }, {
-                name: 'series-projection',
-                data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
-            }]
-        };
-
-        options = {
-            fullWidth: true,
-            lineSmooth: false,
-            height: "270px",
-            low: 0,
-            high: 'auto',
-            series: {
-                'series-projection': {
-                    showArea: true,
-                    showPoint: false,
-                    showLine: false
-                },
-            },
-            axisX: {
-                showGrid: false,
-
-            },
-            axisY: {
-                showGrid: false,
-                onlyInteger: true,
-                offset: 0,
-            },
-            chartPadding: {
-                left: 20,
-                right: 20
-            }
-        };
-
-        new Chartist.Line('#visits-trends-chart', data, options);
-
-
-        // visits chart
-        data = {
-            labels: {!! json_encode($labels) !!},
-            series: [{
-                name: 'HUM SHT',
-                data: {!! json_encode($data1) !!},
-            }, {
-                name: 'TEMPC DS',
-                data: {!! json_encode($data2) !!},
-            }, {
-                name: 'TEMPC SHT',
-                data: {!! json_encode($data3) !!},
-            }]
-        };
-
-        options = {
-            height: 300,
-            axisX: {
-                showGrid: false
-            },
-        };
-
-        new Chartist.Bar('#visits-chart', data, options);
-
-
-        // real-time pie chart
-        var sysLoad = $('#system-load').easyPieChart({
-            size: 130,
-            barColor: function(percent) {
-                return "rgb(" + Math.round(200 * percent / 100) + ", " + Math.round(200 * (1.1 - percent / 100)) + ", 0)";
-            },
-            trackColor: 'rgba(245, 245, 245, 0.8)',
-            scaleColor: false,
-            lineWidth: 5,
-            lineCap: "square",
-            animate: 800
-        });
-
-        var updateInterval = 3000; // in milliseconds
-
-        setInterval(function() {
-            var randomVal;
-            randomVal = getRandomInt(0, 100);
-
-            sysLoad.data('easyPieChart').update(randomVal);
-            sysLoad.find('.percent').text(randomVal);
-        }, updateInterval);
-
-        function getRandomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-
-    });
-    </script>
 @endsection
